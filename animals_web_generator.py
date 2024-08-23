2f12a0f81fde1ed817553497b53a8f4aa4bd04dc
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2024-08-23 10:45:59 +0200

    implementing suggested  single animal serialization

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,26 +7,25 @@ def load_data(file_path):
         return json.load(handle)
 
 
-def convert_to_animals_data(json_content):
+def serialize_animal(animal_obj):
     """
-    Extracting animal details from the given json content.
+    Single animal serialization.
     """
     output = ''
-    for animal in json_content:
-        output += '<li class="cards__item">'
-        if 'name' in animal:
-            output += f'<div class="card__title">{animal['name']}</div>'
-            output += '<p class="cards__text">'
-        if 'characteristics' in animal and 'diet' in animal['characteristics']:
-            output += f'<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n'
-            output += "<br/>"
-        if 'locations' in animal and len(animal['locations']) > 0:
-            output += f'<strong>Location:</strong> {animal['locations'][0]}<br/>\n'
-            output += "<br/>"
-        if 'characteristics' in animal and 'type' in animal['characteristics']:
-            output += f'<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n'
-        output += "</p>"
-        output += "</li>"
+    output += '<li class="cards__item">'
+    output += f'<div class="card__title">{animal_obj['name']}</div>'
+    output += '<p class="cards__text">'
+    if 'characteristics' in animal_obj and 'diet' in animal_obj['characteristics']:
+        output += f'<strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>\n'
+        output += "<br/>"
+    if 'locations' in animal_obj and len(animal_obj['locations']) > 0:
+        output += f'<strong>Location:</strong> {animal_obj['locations'][0]}<br/>\n'
+        output += "<br/>"
+    if 'characteristics' in animal_obj and 'type' in animal_obj['characteristics']:
+        output += f'<strong>Type:</strong> {animal_obj['characteristics']['type']}<br/>\n'
+    output += "</p>\n"
+    output += "</li>\n"
+    print(output)
     return output
 
 
@@ -35,11 +34,13 @@ def replace_animal_info():
      Replace the HTML template __REPLACE_ANIMALS_INFO__ with the result from animals data.
     """
     animals_data = load_data('animals_data.json')
-    actual_animal_data = convert_to_animals_data(animals_data)
+    output = ''
+    for animals_obj in animals_data:
+        output += serialize_animal(animals_obj)
     content_to_be_replaced = "__REPLACE_ANIMALS_INFO__"
     with open("animals_template.html", "r") as filehandle:
         template_content = filehandle.read()
-    modified_content = template_content.replace(content_to_be_replaced, actual_animal_data)
+    modified_content = template_content.replace(content_to_be_replaced, output)
     with open("animals.html", "w") as fileobject:
         fileobject.write(modified_content)
 
